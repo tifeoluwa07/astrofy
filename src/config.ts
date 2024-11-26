177bdbad1847c38c5154cfde8ba19f0888efd4e9
tifeoluwa07 <salakotemmyladeh@gmail.com>
2024-11-26 13:05:58 +0100

    Update config.ts
    meta title 
    meta description

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Salako Ololade |Digital Marketer
-export const SITE_DESCRIPTION = 'Plans and execute all web, SEO/SEM,database marketing,email,social media and display advertising campaigns.
+export const SITE_TITLE = 'Salako Ololade |Digital Marketer';
+export const SITE_DESCRIPTION = 'Plans and execute all web, SEO/SEM,database marketing,email,social media and display advertising campaigns.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
